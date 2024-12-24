68b4a00b0203cae9d18229c02ddf32b4d522827c
Rafael Ferreira Monteiro <rafaelferreirax3@gmail.com>
2024-12-24 18:57:24 -0300

    feat: add subtract

--- index.test.js
+++ index.test.js
@@ -1,5 +1,9 @@
-const sum = require('./index');
+const { sum, subtract} = require("./index");
 
 test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
+});
+
+test('adds 2 - 1 to equal 1', () => {
+    expect(subtract(2, 1)).toBe(1);
 });
